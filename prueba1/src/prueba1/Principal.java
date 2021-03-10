5d8528d53862502b3ce3db3c0223020565617dd7
gperez <gperezp10@iesalbarregas.es>
2021-03-10 18:11:12 +0100

    Validamos clase Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
-		System.out.println("2º commit");
-		System.out.println("3er commit");
+		System.out.println("Hola mundo java-git Versión 2");
+		System.out.println("Hola mundo. Pruebas Git. Se añade Cambio1");
 
 	}
 
